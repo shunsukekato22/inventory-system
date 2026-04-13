d8be7a33971c59f76adf1d093afd3a601948af42
Shunsuke Kato <kyuuri2244@yahoo.co.jp>
2026-04-13 16:26:30 +0900

    web化

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
+from main import InventoryManager
 
 app = Flask(__name__)
+manager = InventoryManager()
 
 @app.route('/')
 def index():
-    return 'Hello, Flask!'
+    return render_template('index.html', inventory=manager.inventory)
 
 if __name__ == '__main__':
     app.run(debug=True)
